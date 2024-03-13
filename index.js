6f0433f13e7ceca826ffeda9fe99ee788f0bebd1
Jacob Higham <149442786+JakeHigham@users.noreply.github.com>
2024-03-12 19:45:53 -0500

    Added starting files
    Added the starting files, still need to finish /lib and /db

---
+++ index.js
@@ -0,0 +1,9 @@
+const selectTask = require('./lib/selectTask.js');
+
+// Starts the application and presents the task list prompt
+function init() {
+    selectTask();
+}
+
+// Call the init function directly
+init();
